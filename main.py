cd529c4b0167532e53dedfc6786ec2b8c2054660
Paul Theceri <paul.theceri@gmail.com>
2022-01-22 20:46:52 +0300

    M-Pesa integration

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 from apscheduler.schedulers.background import BackgroundScheduler
 import datetime
-import requests,json
+import requests
+import json
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
 from flask_cors import CORS
+from mpesa import *
 
 app = Flask(__name__)
 alvapi_key = "EZKBB11OQDGS3BGK"
@@ -15,59 +17,92 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 cors = CORS(app, resources={r"*": {"origins": "*"}})
 
-class Forex(db.Model):
-    __tablename__ = 'forex'
 
-    id = db.Column(db.Integer, primary_key=True)
-    symbol = db.Column(db.String(80), nullable=False)
-    data = db.Column(db.JSON, unique=True)
-    created_date  = db.Column(db.DateTime, nullable=False, server_default=func.now())
-    
-@app.before_first_request
-def create_tables():
-    db.create_all()
-    print("Tables created")
+# class Forex(db.Model):
+#     __tablename__ = 'forex'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     symbol = db.Column(db.String(80), nullable=False)
+#     data = db.Column(db.JSON, unique=True)
+#     created_date = db.Column(db.DateTime, nullable=False, server_default=func.now())
+
+
+# @app.before_first_request
+# def create_tables():
+#     # db.create_all()
+#     print("Tables created")
+
 
 @app.route("/")
 def index():
-   return "This is a private API"
+    return "This is a private API"
 
-@app.route("/json/forex")
-def forex_api():
-    #Get from DB
-    data_json=Forex.query.all()
-    import ast
-    res =json.dumps([ast.literal_eval(d.data) for d in data_json])
-    return res    
- 
-def request_scheduler():
-    # try:
-        # Make a GET Request to Alphavantage
-        r = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=IBM&apikey=' + alvapi_key)
+# @app.route("/json/forex")
+# def forex_api():
+#     # Get from DB
+#     data_json = Forex.query.all()
+#     import ast
+#     res = json.dumps([ast.literal_eval(d.data) for d in data_json])
 
-        #Store the data received
-        data = r.text
+#     return res
 
-        # find out the type of the data
-        print(type(data))
+# M-Pesa routes
+@app.route("/stkpush")
+def stk_push():
+    phone_number = '254727359578'
+    account_number = '25747'
+    amount = 1
+    header = {'Authorization': 'Bearer %s' % authenticator()}
+    url = base_url + 'mpesa/stkpush/v1/processrequest'
+    body = {
+        "BusinessShortCode": business_short_code,
+        "Password": generate_password(),
+        "Timestamp": get_timestamp(),
+        "TransactionType": "CustomerPayBillOnline",
+        "Amount": amount,
+        "PartyA": phone_number,
+        "PartyB": business_short_code,
+        "PhoneNumber": phone_number,
+        "CallBackURL": "https://mydomain.com/pat",
+        "AccountReference": account_number,
+        "TransactionDesc": "Edwin is shouting at us"
+    }
+    r=requests.post(url, json=body, headers=header)
 
-        #Convert from string to a dictionary using json.loads()
-        data_json = json.loads(data)
+    print(r.json())
+    return "This shit is not working"
 
-        #Find out type of data
-        print("From scheduler:",type(data_json))
 
-        data = Forex(symbol="IBM", data=json.loads(json.dumps(data_json)))
-        db.session.add(data)
-        db.session.commit()
-    # except Exception as e:
-    #     print("Error in scheduler:", e)
+# def request_scheduler():
+#     # try:
+#     # Make a GET Request to Alphavantage
+#     r = requests.get(
+#         'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=IBM&apikey=' + alvapi_key)
 
-#Create the scheduler job
-scheduler.add_job(request_scheduler, 'interval', minutes=0.25)
+#     # Store the data received
+#     data = r.text
 
-#start the scheduler
-scheduler.start()    
+#     # find out the type of the data
+#     print(type(data))
+
+#     # Convert from string to a dictionary using json.loads()
+#     data_json = json.loads(data)
+
+#     # Find out type of data
+#     print("From scheduler:", type(data_json))
+
+#     data = Forex(symbol="IBM", data=json.loads(json.dumps(data_json)))
+#     db.session.add(data)
+#     db.session.commit()
+#     # except Exception as e:
+#     #     print("Error in scheduler:", e)
+
+
+# Create the scheduler job
+# scheduler.add_job(request_scheduler, 'interval', minutes=0.25)
+
+# start the scheduler
+# scheduler.start()
 
 if __name__ == "__main__":
     app.run()
---
+++ mpesa.py
@@ -0,0 +1,52 @@
+import requests
+from requests.auth import HTTPBasicAuth
+from datetime import datetime
+import base64
+
+# create a base_url so we do not keep repeating links that have the same base
+base_url = 'https://sandbox.safaricom.co.ke/'
+
+# copy the consumer key provided when you create an app in Daraja
+consumer_key = '4QrDNOo1NQh6ITKSEop05OUHVwm9YOoG'
+
+# copy the consumer secret provided when you create an app in Daraja
+consumer_secret = 'NBlarPMldvJmJUPm'
+
+# the business short code is the one provided in the Lipa Na M-Pesa Online API section of the documentation (https://developer.safaricom.co.ke/Documentation#:~:text=%C2%A0%22BusinessShortCode%22%3A%22174379%22%2C%C2%A0%20%C2%A0)
+business_short_code = "174379"
+
+passkey = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919'
+
+def authenticator():
+    r = requests.get(base_url+'oauth/v1/generate?grant_type=client_credentials', auth=HTTPBasicAuth(consumer_key, consumer_secret))
+
+    # since r.json() returns a dictionary (something like ), we use the access_token key to get the token
+    return r.json()['access_token']
+
+def get_timestamp():
+    # use the datetime library to get the current time
+    sai = datetime.now()
+
+    # format the string to look like the one provided in the Lipa Na M-Pesa Online API section of the documentation here: https://developer.safaricom.co.ke/Documentation#:~:text=%22Timestamp%22%3A%2220160216165627%22%2C%C2%A0%20%C2%A0%C2%A0
+    sai_string = sai.strftime("%Y%m%d%H%M%S")
+    
+    return sai_string
+
+def generate_password():
+    # this is the password used for encrypting the request sent: A base64 encoded string. The base4 encoded string is a combination of Shortcode + Passkey + Timestamp
+    # from the documentation, the password that we are going to encrypt is a combination of the business shortcode, the passkey, and the current timestamp
+    password_to_encrypt = business_short_code + passkey + get_timestamp()
+
+    # we then encode the password using base64
+    encrypted_password = base64.b64encode(password_to_encrypt.encode())
+
+    # we then decode the password to get the actual password
+    decoded_password = encrypted_password.decode('utf-8')
+
+    return decoded_password
+
+
+
+authenticator()
+get_timestamp()
+generate_password()
